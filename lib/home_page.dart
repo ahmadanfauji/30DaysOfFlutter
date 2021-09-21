b214d98f7c77615adc8e7e8625c11ab4f8dd21db
Ahmad Fauji <ahmadanfauji@gmail.com>
2021-09-21 20:28:07 +0900

    Day 2 Completed.

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final int days = 2;
+    final String name = "Ahmad";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Day 2 App'),
+      ),
+      body: Center(
+        child: Container(
+          child:
+              Text('Selamat datang di flutter hari ke $days, semangat $name !'),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:first_project/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,18 +6,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text('Welcome brother to my first app.'),
-          ),
-        ),
-      ),
-    );
+    return MaterialApp(home: HomePage());
   }
 }
